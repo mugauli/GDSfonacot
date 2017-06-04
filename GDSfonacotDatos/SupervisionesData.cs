9fe0f20f67609e4e238933a0ec3a200deaa86161
Chucho <64464@IXTAPALUCA>
2017-06-03 20:36:56 -0500

    Modelo de datos

---
+++ GDSfonacotDatos/SupervisionesData.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static GDSfonacotDatos.Utilities.Util;
+
+namespace GDSfonacotDatos
+{
+    public class SupervisionesData
+    {
+        public MethodResponse<int> GuardarSupervision(Historial_de_Supervisiones supervision)
+        {
+
+            try
+            {
+                using (var context = new GDSfonacotEntities())
+                {
+                    var response = new MethodResponse<int> { Code = 0 };
+
+                    var usuariosDB = context.Historial_de_Supervisiones.Add(supervision);
+                    context.SaveChanges();
+
+                    return response;
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                return new MethodResponse<int> { Code = -100, Message = ex.Message };
+            }
+        }
+
+        public MethodResponse<List<Historial_de_Supervisiones>> ObtenerSupervisones(int IdSupervisiones, string filter)
+        {
+            try
+            {
+                using (var context = new GDSfonacotEntities())
+                {
+                    var response = new MethodResponse<List<Historial_de_Supervisiones>> { Code = 0 };
+
+                    var usuariosDB = context.Historial_de_Supervisiones
+                        .Join(context.Sucursales, s => s.IdSucursal, c => c.IdSucursal,(s, c) => new { s, c })
+                        .Where(sc => (sc.s.IdSupervisiones == IdSupervisiones || IdSupervisiones == 0) && (sc.c.DescripcionSucursal.Contains(filter) || filter == "")).Select(x => x.s).ToList();
+
+                    response.Result = usuariosDB;
+
+                    return response;
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                return new MethodResponse<List<Historial_de_Supervisiones>> { Code = -100, Message = ex.Message };
+            }
+        }
+
+        public MethodResponse<List<Usuarios>> ObtenerUsuarios(int IdUsuario) {
+            
+            try
+            {
+                
+
+                using (var context = new GDSfonacotEntities())
+                {
+                    var response = new MethodResponse<List<Usuarios>> { Code = 0 };
+
+                    var usuariosDB = context.Usuarios.Where(x => x.IdUsuario == IdUsuario || IdUsuario == 0).ToList();
+
+                    response.Result = usuariosDB;
+
+                    return response;
+                }
+                   
+            }
+            catch (Exception ex)
+            {
+
+                return new MethodResponse<List<Usuarios>> { Code = -100, Message = ex.Message };
+            }
+        }
+
+        public MethodResponse<Usuarios> LoginUsuario(string Usuario ,string password)
+        {
+
+            try
+            {
+
+
+                using (var context = new GDSfonacotEntities())
+                {
+                    var response = new MethodResponse<Usuarios> { Code = 0 };
+
+                    var usuariosDB = context.Usuarios.Where(x => x.Usuario == Usuario || x.Pass == password).SingleOrDefault();
+
+                    if(usuariosDB != null) response.Result = usuariosDB;
+
+                    return response;
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                return new MethodResponse<Usuarios> { Code = -100, Message = ex.Message };
+            }
+        }
+
+        public MethodResponse<List<Sucursales>> ObtenerSucursales(int IdSucursal)
+        {
+
+            try
+            {
+                using (var context = new GDSfonacotEntities())
+                {
+                    var response = new MethodResponse<List<Sucursales>> { Code = 0 };
+
+                    var usuariosDB = context.Sucursales.Where(x => x.IdSucursal == IdSucursal || IdSucursal == 0).ToList();
+
+                    response.Result = usuariosDB;
+
+                    return response;
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                return new MethodResponse<List<Sucursales>> { Code = -100, Message = ex.Message };
+            }
+        }    
+
+    }
+}
---
+++ GDSfonacotDatos/Utilities/Util.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDSfonacotDatos.Utilities
+{
+    public class Util
+    {
+        public class MethodResponse<T>{
+
+            public int Code { get; set; }
+
+            public string Message { get; set; }
+
+            public T Result { get; set; }
+        }
+    }
+}
