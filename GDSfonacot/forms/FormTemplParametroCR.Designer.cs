1c3ab3e5ab718fec4b7bca966b816c1a8d433801
spiceboyjebus <jesusmanuelsanchezmendoza@gmail.com>
2017-06-21 23:52:06 -0500

    El cambio se realizo en el archivo tal y se agregaron 10 cosas mas

--- GDSfonacot/forms/FormTemplParametroCR.Designer.cs
+++ GDSfonacot/forms/FormTemplParametroCR.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.textparam = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textparam
@@ -49,11 +50,22 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Ingresa el numero de supervision completo y presiona enter";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(132, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // FormTemplParametroCR
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(311, 175);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textparam);
             this.MaximizeBox = false;
@@ -69,5 +81,6 @@
 
         private System.Windows.Forms.TextBox textparam;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- GDSfonacot/forms/FormTemplParametroCR.cs
+++ GDSfonacot/forms/FormTemplParametroCR.cs
@@ -31,5 +31,10 @@ namespace GDSfonacot.forms
 
             }
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
