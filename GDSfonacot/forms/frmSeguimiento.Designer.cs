8bc8f382196dab96b14e75336059b41024db64fe
spiceboyjebus <jesusmanuelsanchezmendoza@gmail.com>
2017-07-17 12:10:38 -0500

    Agregar nuevo formulario de seguimiento

--- GDSfonacot/forms/frmSeguimiento.Designer.cs
+++ GDSfonacot/forms/frmSeguimiento.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmSeguimiento
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(925, 306);
+            this.Name = "frmSeguimiento";
             this.Text = "frmSeguimiento";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
